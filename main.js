9a7a4069f3598fce0f883b46ebdfd305affa44c7
k3jo <joanne.kwh@gmail.com>
2015-09-16 16:16:39 -0400

    updated project

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('.photo-trigger').click(function() {
+    $('.gallery-filter ul li a').slideToggle(500);
+  });//end slide toggle
+  
+  $(window).resize(function() {		
+		if (  $(window).width() > 500 ) {			
+			$('.gallery-filter ul li a').removeAttr('style');
+		 }
+	});//end resize
+
+  $('.menu-trigger').click(function() {
+    $('.responsive-nav').slideToggle(500);
+  });//end slide toggle
+
+});//end ready
